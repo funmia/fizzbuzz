f6712b351f5c100fd788b8f909d09728b9863186
Funmi Adewodu <funmiadewodu@yahoo.com>
2017-06-28 23:52:20 +0000

    Updates fizzbuzz_spec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,23 +9,11 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end 
   
-  it 'returns "fizzbuzz" when passed 15' do 
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do 
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end 
   
-  it 'returns given number when the number is not a multiple of 3 and 5' do 
+  it 'returns given number when the number is not a multiple of 3 or 5' do 
     expect(fizzbuzz(4)).to eq 4
   end
-  
-  it 'returns "fizz" when passed 9' do 
-    expect(fizzbuzz(9)).to eq 'fizz'
-  end 
-  
-  it 'returns "buzz" when passed 25' do
-    expect(fizzbuzz(25)).to eq 'buzz'
-  end 
-  
-  it 'returns "buzz" when passed 100' do 
-    expect(fizzbuzz(100)).to eq 'buzz'
-  end 
 end 
